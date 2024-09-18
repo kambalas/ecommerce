32edb0c77a3450bbb434a88564cb01a011e63a1b
Motiejūnas Ugnius <motiejunasugnius@gmail.com>
2024-09-18 13:01:10 +0300

    Implement Category view with full CRUD operations

---
+++ Models/Category.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace ecommerce.Models
+{
+    public class Category
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        [DisplayName("Display Order")]
+        [Range(1,100, ErrorMessage ="Display order must be between 1-100")]
+        public int DisplayOrder { get; set; }
+    }
+}
